418e9737d16c33e463426353c70952758f9dcd1e
interceptor128 <polestar.interceptor.0128@gmail.com>
2022-04-14 20:36:11 +0000

    add prism.js programming component

--- plugins/prism.js
+++ plugins/prism.js
@@ -1,6 +1,8 @@
 import Prism from 'prismjs'
 import 'prismjs/themes/prism-okaidia.css'
-import 'prismjs/components/prism-***'
+import 'prismjs/components/prism-abap.min.js'
+import 'prismjs/components/prism-python.min.js'
+import 'prismjs/components/prism-typescript.min.js'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.min.js'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 export default Prism
